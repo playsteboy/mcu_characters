94c3604c8ca578423ed9b58bf29e8801fbcbe536
Mikaiah <mikaiahranaivoson@gmail.com>
2025-08-06 13:30:13 +0300

    add front folder

---
+++ front/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import './index.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <p className='text-amber-500 font-bold'>bonjour</p>
+    </>
+  )
+}
+
+export default App
