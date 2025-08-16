ce2f8fcec7434b59c7ac2624ce7634d68b8dfdf6
Mikaiah <mikaiahranaivoson@gmail.com>
2025-08-16 07:52:35 +0300

    correction on header

--- front/src/components/header.jsx
+++ front/src/components/header.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default function Header({err}){
+export default function Header(){
     return(
         <header className="flex flex-row justify-center items-center w-full h-48 bg-[#0B305B] ">
             <h1 className="flex flex-col w-1/2 h-1/2 justify-center text-center text-[#ED1D24] text-5xl font-bold">Marvel Characters</h1>
